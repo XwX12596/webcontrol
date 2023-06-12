40e22f9f139194acc7ab228aa852ef0c2d150362
XwX12596 <1259685860@qq.com>
2023-06-12 21:47:47 +0800

    dev

--- host.py
+++ host.py
@@ -24,78 +24,78 @@ class StreamingOutput(object):
                 self.condition.notify_all()
             self.buffer.seek(0)
         return self.buffer.write(buf)
+#
+# class picam_server():
+#     def __init__(self):
+#         gs90_angle(45)
+#         time.sleep(0.3)
+#         gs90_angle('stop')
+#         self.fetchTime = 5
 
-class picam_server():
-    def __init__(self):
-        gs90_angle(45)
-        time.sleep(0.3)
-        gs90_angle('stop')
-        self.fetchTime = 5
+@route('/')
+def index():
+    response.status = 301
+    response.set_header('Location', '/index.html')
+    return
 
-    @route('/')
-    def index():
-        response.status = 301
-        response.set_header('Location', '/index.html')
-        return
+@route('/index.html')
+def html_page():
+    return template("index")
 
-    @route('/index.html')
-    def html_page():
-        return template("index")
+# @post('/fetch')
+# def picture():
+#     fetch()
 
-    # @post('/fetch')
-    # def picture():
-    #     fetch()
+@post('/warning')
+def bell():
+    warning()
 
-    @post('/warning')
-    def bell():
-        warning()
+@post('/<angle:re:[0-9]+>')
+def moveCam(angle):
+    print(angle)
+    gs90_angle(int(angle))
+    time.sleep(0.3)
+    gs90_angle('stop')
+    gs90_pwm.stop()
 
-    @post('/<angle:re:[0-9]+>')
-    def moveCam(angle):
-        print(angle)
-        gs90_angle(int(angle))
-        time.sleep(0.3)
-        gs90_angle('stop')
-        gs90_pwm.stop()
-    
-    @post('/<time:re:updateWait[0-9]*>')
-    def updateWait(time):
-        print(time)
-        self.fetchTime = time
+@post('/<time:re:updateWait[0-9]*>')
+def updateWait(time):
+    print(time)
+    self.fetchTime = time
 
-    @route('/stream.mjpg')
-    def stream():
-        def generate():
-            while True:
-                print("1")
-                with output.condition:
-                    output.condition.wait()
-                    frame = output.frame
-                print("2")
-                yield b'--FRAME\r\n'
-                yield b'Content-Type: image/jpeg\r\n'
-                yield b'Content-Length: ' + str(len(frame)).encode() + b'\r\n\r\n'
-                yield frame
-                yield b'\r\n'
-        response.status = 200
-        response.set_header('Age', '0')
-        response.set_header('Cache-Control', 'no-cache, private')
-        response.set_header('Pragma', 'no-cache')
-        response.set_header('Content-Type', 'multipart/x-mixed-replace; boundary=FRAME')
-        return generate()
-
-    def host(self):
-        with picamera.PiCamera(resolution='640x480', framerate=24) as camera:
-            output = StreamingOutput()
-            camera.start_recording(output, format='mjpeg')
-            run(host='0.0.0.0', port=8000)
-            camera.stop_recording()
-
-    def timer(self):
+@route('/stream.mjpg')
+def stream():
+    def generate():
         while True:
-            time.sleep(self.fetchTime)
-            #fetch()
-            print("autoFetching")
+            print("1")
+            with output.condition:
+                output.condition.wait()
+                frame = output.frame
+            print("2")
+            yield b'--FRAME\r\n'
+            yield b'Content-Type: image/jpeg\r\n'
+            yield b'Content-Length: ' + str(len(frame)).encode() + b'\r\n\r\n'
+            yield frame
+            yield b'\r\n'
+    response.status = 200
+    response.set_header('Age', '0')
+    response.set_header('Cache-Control', 'no-cache, private')
+    response.set_header('Pragma', 'no-cache')
+    response.set_header('Content-Type', 'multipart/x-mixed-replace; boundary=FRAME')
+    return generate()
+
+def host(self):
+    with picamera.PiCamera(resolution='640x480', framerate=24) as camera:
+        output = StreamingOutput()
+        camera.start_recording(output, format='mjpeg')
+        run(host='0.0.0.0', port=8000)
+        camera.stop_recording()
+
+# def timer(self):
+#     while True:
+#         time.sleep(self.fetchTime)
+#         #fetch()
+#         print("autoFetching")
 
     # def start(self):
     #     t1 = threading.Thread(target=self.host)
@@ -112,5 +112,4 @@ class picam_server():
     #             sys.exit(0)
 
 if __name__ == '__main__':
-    server = picam_server()
-    server.host()
+    host()
