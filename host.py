a8e2d79c101a303e0ee1f9e5bd716f85a198c835
XwX12596 <1259685860@qq.com>
2023-06-12 21:53:04 +0800

    dev

--- host.py
+++ host.py
@@ -10,21 +10,22 @@ from motor import gs90_angle
 from bell import warning
 from threading import Condition
 
-def stream():
-    class StreamingOutput(object):
-        def __init__(self):
-            self.frame = None
-            self.buffer = io.BytesIO()
-            self.condition = Condition()
+class StreamingOutput(object):
+    def __init__(self):
+        self.frame = None
+        self.buffer = io.BytesIO()
+        self.condition = Condition()
 
-        def write(self, buf):
-            if buf.startswith(b'\xff\xd8'):
-                self.buffer.truncate()
-                with self.condition:
-                    self.frame = self.buffer.getvalue()
-                    self.condition.notify_all()
-                self.buffer.seek(0)
-            return self.buffer.write(buf)
+    def write(self, buf):
+        if buf.startswith(b'\xff\xd8'):
+            self.buffer.truncate()
+            with self.condition:
+                self.frame = self.buffer.getvalue()
+                self.condition.notify_all()
+            self.buffer.seek(0)
+        return self.buffer.write(buf)
+
+def stream():
     with picamera.PiCamera(resolution='640x480', framerate=24) as camera:
             output = StreamingOutput()
             camera.start_recording(output, format='mjpeg')
