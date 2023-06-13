55d0553afa4ddc58c5335a5660bb1c1322ca1931
XwX12596 <1259685860@qq.com>
2023-06-13 10:57:41 +0800

    dev

--- host.py
+++ host.py
@@ -10,7 +10,20 @@ from motor import gs90_angle
 from bell import warning
 from threading import Condition
 
+class StreamingOutput(object):
+    def __init__(self):
+        self.frame = None
+        self.buffer = io.BytesIO()
+        self.condition = Condition()
 
+    def write(self, buf):
+        if buf.startswith(b'\xff\xd8'):
+            self.buffer.truncate()
+            with self.condition:
+                self.frame = self.buffer.getvalue()
+                self.condition.notify_all()
+            self.buffer.seek(0)
+        return self.buffer.write(buf)
 
 # class picam_server():
 #     def __init__(self):
